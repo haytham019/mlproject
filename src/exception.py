5449ceccf4a72769a026e20e47201444cc29f2be
haytham019 <haythemhy22@gmail.com>
2026-01-04 21:52:52 +0100

    perform EDA and create MODEL_TRAINING file

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,6 @@
 import sys 
 import logging 
-
+from logger import logging
 
 def error_message_details(error , error_detail: sys) :
     _,_,exc_tb = error_detail.exc_info()
@@ -14,4 +14,3 @@ class CustomException(Exception) :
         self.error_message = error_message_details(error_message , error_detail=error_detail)
     def __str__(self) :
         return self.error_message
-
